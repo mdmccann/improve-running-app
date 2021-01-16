9c1976d5c11532482bc0e8feb61426c8f8963730
William Cheung <william.cheung.c@gmail.com>
2021-01-16 22:32:18 +0000

    tidied up model a bit

--- src/app/models/plan.ts
+++ src/app/models/plan.ts
@@ -1,48 +1,22 @@
+import { Activity } from "./Activity";
+
 export interface WeeklyPlan {
   weekStarting: Date;
   days: DayPlan[];
 }
 
 export type DayPlan = {
-  day: 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sunday';
-  activity: Run | Rest;
+  day: Day
+  activity: Activity;
   achieved: boolean;
   briefDescription: string;
 };
 
-type Run = TimedEasyRun | DistancedEasyRun | TimedTempoRun | DistancedTempoRun | IntervalRun;
-type Rest = null;
-
-type TimedEasyRun = {
-  type: 'TimedEasyRun';
-  duration: Seconds;
-}
-
-type DistancedEasyRun = {
-  type: 'DistancedEasyRun';
-  distance: Metres;
-}
-
-type TimedTempoRun = {
-  type: 'TimedTempoRun';
-  duration: Seconds;
-  pace: MinutePerKm;
-}
-
-type DistancedTempoRun = {
-  type: 'DistancedTempoRun';
-  distance: Metres;
-  pace: MinutePerKm;
-}
-
-type IntervalRun = {
-  type: 'IntervalRun';
-  reps: number;
-  distance: Metres;
-  pace: MinutePerKm;
-  restTime: Seconds;
-}
-
-type Seconds = number;
-type Metres = number;
-type MinutePerKm = number;
+type Day
+  = 'monday'
+  | 'tuesday'
+  | 'wednesday'
+  | 'thursday'
+  | 'friday'
+  | 'saturday'
+  | 'sunday';
